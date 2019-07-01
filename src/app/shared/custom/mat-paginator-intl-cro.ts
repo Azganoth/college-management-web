fdd2717601fcd22806dfb6207f4aa8fba6000ae1
Ademir Ferreira <ademir.ferreiracif@gmail.com>
2019-07-01 12:42:07 -0300

    fix: add material table missing translations

--- src/app/shared/custom/mat-paginator-intl-cro.ts
+++ src/app/shared/custom/mat-paginator-intl-cro.ts
@@ -1,9 +1,10 @@
 import { MatPaginatorIntl } from '@angular/material';
 
 export class MatPaginatorIntlCro extends MatPaginatorIntl {
-  itemsPerPageLabel = 'Itens por pagina';
+  itemsPerPageLabel = 'Itens por pagina:';
   firstPageLabel = 'Primeira página';
   lastPageLabel = 'Última página';
   nextPageLabel = 'Próxima página';
   previousPageLabel = 'Página anterior';
+  getRangeLabel = (page, pageSize, length) => `${page} de ${length}`;
 }
